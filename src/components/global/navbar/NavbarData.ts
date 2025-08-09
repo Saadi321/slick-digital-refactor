c40323a47ccc69a13c3760951c552889921276ac
Muhammad Saad Satti <88727023+Saadi321@users.noreply.github.com>
2025-08-09 14:40:16 +0500

    Update NavbarData.ts

--- src/components/global/navbar/NavbarData.ts
+++ src/components/global/navbar/NavbarData.ts
@@ -1,10 +1,9 @@
 import { NavItem } from './NavbarTypes';
 
 export const navItems: NavItem[] = [
-  { href: "#home", label: "Home" },
-  { href: "#services", label: "Services" },
-  { href: "#case-studies", label: "Case Studies" },
-  { href: "#testimonials", label: "Testimonials" },
-  { href: "#careers", label: "Careers" },
-  { href: "#contact", label: "Contact" }
+  { href: "/", label: "Home" },
+  { href: "/services", label: "Services" },
+  { href: "/projects", label: "Projects" },
+  { href: "/about", label: "About" },
+ { href: "/contact", label: "Contact" }
 ];
